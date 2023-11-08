29186070efd7a60576d3af30ba222604142cda22
Sumit Kumar <sumitkumar84390@gmail.com>
2023-11-08 17:08:46 +0530

    second program of this assignent is complete third program is in under process

--- src/arrayPrograms/Arrays_2.java
+++ src/arrayPrograms/Arrays_2.java
@@ -1,8 +1,15 @@
 package arrayPrograms;
 
+import javax.management.Query;
+
+//	https://drive.google.com/file/d/1Q1YIj7mywtWxGhfxXd8w9cl51oiHBYJo/view?usp=sharing
+
 public class Arrays_2 {
 	public static void main(String[] args) {
 		Arrays_2 arrays_2 = new Arrays_2();
+		int a[]= {1,2,3,5,6,7,8};
+//		System.out.println(arrays_2.union(a, b));
+//		System.out.println(arrays_2.smallestInteger(a, 4));
 	}
 
 //	Que 1. Given an unsorted array arr[] of size N having both negative and positive integers, place
@@ -32,5 +39,50 @@ public class Arrays_2 {
 //			the arrays. If there are repetitions, then only one occurrence of element should be printed
 //			in the union.
 	
-//	Note Que 2 will complete in next day so you can see it tomorrow.
+	public int union(int[] a,int[] b) {
+		int ans[]=new int[a.length+b.length];
+		int index=0;
+		Arrays_2 arrays_2 = new Arrays_2();
+		for (int i : a) {
+			ans[index++]=i;
+		}
+		for (int i : b) {
+			if(arrays_2.findElement(a, i)) {
+				ans[index++]=i;
+			}
+		}
+		return index-1;
+	}
+//	This is the helper method for union method 
+//	which is use to find the element is present in second array or not.
+	private boolean findElement(int []arr,int elem) {
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==elem) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+//	Que 3. Given an array arr[] and an integer K where K is smaller than size of array, the task is to
+//	find the Kth smallest element in the given array. It is given that all array elements are
+//	distinct.
+	
+//	public int smallestInteger(int arr[],int k) {
+//		int small=Integer.MAX_VALUE;
+//		for(int i=0;i<k;i++) {
+//			if(arr[i]>small) {
+//				small=arr[i];
+//				arr[i]=Integer.MAX_VALUE;
+//			}
+//		}
+//		for(int i=0;i<k;i++) {
+//			if(arr[i]>small) {
+//				small=arr[i];
+//				arr[i]=Integer.MAX_VALUE;
+//			}
+//		}
+//		return small;
+//	}
+	
 }
